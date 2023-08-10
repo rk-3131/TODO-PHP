d1344efd960dd6f5586d2ce66a5c87c7898ef6f9
Radhakrushna Mahadik <mahdikrs512@gmail.com>
2023-08-10 22:15:37 +0530

    data table feature has been added into the database

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+        $insert = false;
         // we will connect to the database here
         $host = "localhost";
         $user = "root";
@@ -13,6 +14,7 @@
         }else{
           // echo "Database connected successfully";
 
+          $insert = true;
           if ($_SERVER["REQUEST_METHOD"] == "POST"){
             $title = $_POST['title'];
             $desc = $_POST['desc'];
@@ -20,8 +22,6 @@
             $sql = "INSERT INTO `notes` (`Title`, `Description`) VALUES ('$title', '$desc')";
 
             $result = mysqli_query($conn, $sql);
-
-
           }
           
         }
@@ -33,8 +33,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>TODO</title>
+  <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    
 </head>
 
 <body>
@@ -64,6 +66,12 @@
       </div>
     </div>
   </nav>
+  <?php
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Success!</strong> Your Note has been added to the database.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>'
+?>
   <div class="container my-3">
     <h2>Add a Note</h2>
     <form action="index.php" method = "post">
@@ -80,6 +88,16 @@
     </form>
   </div>
   <div class="container">
+    <table class='table' id="myTable">
+  <thead>
+    <tr>
+      <th scope='col'>Sr.No</th>
+      <th scope='col'>Title</th>
+      <th scope='col'>Description</th> 
+      <th scope='col'>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
     <?php
         $sql = "SELECT * FROM `notes`";
 
@@ -89,38 +107,30 @@
           echo "Error while fetching data";
         }else{
           $num = mysqli_num_rows($result);
-
-
-          echo "<table class='table'>
-  <thead>
-    <tr>
-      <th scope='col'>Sr.No</th>
-      <th scope='col'>Title</th>
-      <th scope='col'>Description</th> 
-      <th scope='col'>Actions</th>
-    </tr>
-  </thead>
-  <tbody>";
-
+          $sno = 1;
           if ($num > 0){
             while ($row = mysqli_fetch_assoc($result)){
               echo " <tr>
-              <td> " . $row['Sr.No'] . " </td>
+              <td> " . $sno++ . " </td>
               <td> " . $row['Title'] . " </td>
               <td> " . $row['Description'] . " </td>
               <td> " . "Actions" . " </td>
             </tr> ";
-            
-  echo "<br>";
-  "</tbody> </table>";
             }
           }
         }
       ?>
+      </tbody> </table>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
     crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js">
+    </script>
+    <script>
+      let table = new DataTable('#myTable');
+    </script>
 </body>
 
 </html>
